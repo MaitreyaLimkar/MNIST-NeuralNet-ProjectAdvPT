5823c89414f8b4f76279e0f0ee9416bd2f3d8215
Maitreya L. <maitu.clever@gmail.com>
2025-01-22 15:07:29 +0100

    Test commit

---
+++ src/delete.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Maitreya Limkar on 22-01-2025.
+//
+
+#include "delete.hpp"
---
+++ src/delete.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Maitreya Limkar on 22-01-2025.
+//
+
+#ifndef DELETE_HPP
+#define DELETE_HPP
+
+
+
+class delete {
+
+};
+
+
+
+#endif //DELETE_HPP
